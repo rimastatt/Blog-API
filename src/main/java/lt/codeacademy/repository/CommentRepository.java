9fa98ae08a4aafe53a644f4ffb809bb7e3f6c92a
Rimas <rimastatt@gmail.com>
2020-07-05 12:49:42 +0300

    Implemented CommentService class

--- src/main/java/lt/codeacademy/repository/CommentRepository.java
+++ src/main/java/lt/codeacademy/repository/CommentRepository.java
@@ -4,9 +4,10 @@ import lt.codeacademy.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-
-
+    List<Comment> findCommentsByArticleId(Long id);
 }
--- src/main/java/lt/codeacademy/service/CommentService.java
+++ src/main/java/lt/codeacademy/service/CommentService.java
@@ -1,17 +1,35 @@
 package lt.codeacademy.service;
 
+import lt.codeacademy.entity.Article;
+import lt.codeacademy.entity.Comment;
+import lt.codeacademy.entity.User;
 import lt.codeacademy.repository.CommentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentService {
 
-
     private CommentRepository commentRepository;
+    private ArticleService articleService;
 
-    public CommentService(CommentRepository commentRepository){
+    public CommentService(CommentRepository commentRepository, ArticleService articleService){
         this.commentRepository = commentRepository;
+        this.articleService = articleService;
     }
 
+    public List<Comment> getCommentsByArticleId(Long articleId){
+       return commentRepository.findCommentsByArticleId(articleId);
+    }
 
+    public void deleteComment(Long id){
+        commentRepository.deleteById(id);
+    }
+
+    public void saveOrUpdateComment(User user, Comment comment, Long articleId){
+        Article article = articleService.getArticleById(articleId);
+        comment.setUser(user);
+        comment.setArticle(article);
+    }
 }
